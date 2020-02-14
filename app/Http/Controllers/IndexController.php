026eb1c1d42c6750def0738100eab3af18a6ca48
Obleac Milena <emilia240101@gmail.com>
2020-02-14 11:04:05 +0200

    iojk

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Article;
+
+class IndexController extends Controller
+{
+    //
+    public function index(){
+        $hw='Hello world!';
+        $ms='This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.';
+        $articles=Article::select(['title', 'description', 'img', 'id'])->get();
+        return view('index')->with(['hw'=>$hw, 
+                                    'ms'=>$ms, 
+                                    'articles'=>$articles]);
+        
+    }
+}
